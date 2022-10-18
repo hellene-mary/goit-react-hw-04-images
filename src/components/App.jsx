b357e8c07b8bd76ca385df67cc2322baec08e2ec
Grek Maria <grekmariaa@gmail.com>
2022-10-18 15:19:19 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { Button } from './Button/Button';
+import { ImageGallery } from './imageGallery/ImageGallery';
+import { Loader } from './Loader/Loader';
+// import { Modal } from './Modal/Modal';
+import { Searchbar } from './searchbar/Searchbar';
 
 class App extends Component {
     state = {};
 
+    // отримання значення інпуту пошуку
+    trackingSearch = evt => {
+        evt.preventDefault();
+
+        const form = evt.currentTarget;
+        const searchValue = form.elements.search.value;
+        console.log('searchValue', searchValue);
+
+        form.reset();
+    };
+
     render() {
-        return <></>;
+        return (
+            <>
+                <Searchbar onSubmit={this.trackingSearch} />
+                <ImageGallery />
+                <Loader />
+                <Button />
+                {/* <Modal/> */}
+            </>
+        );
     }
 }
 
