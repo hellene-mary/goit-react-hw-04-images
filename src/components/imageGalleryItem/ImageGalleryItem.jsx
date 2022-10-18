7af860c5d5524abea77c8bf9b982f23c1d242852
Grek Maria <grekmariaa@gmail.com>
2022-10-18 15:07:44 +0300

    Create ImageGalleryItem.jsx

---
+++ src/components/imageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,9 @@
+import css from './ImageGalleryItem.module.css';
+
+export function ImageGalleryItem() {
+    return (
+        <li className="ImageGalleryItem">
+            <img src="" alt="" />
+        </li>
+    );
+}
