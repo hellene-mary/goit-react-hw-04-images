1a54b664d95b5a38b8ac8f8608816f405c3b08a5
Grek Maria <grekmariaa@gmail.com>
2022-10-22 16:38:53 +0300

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,52 @@
-// import css from './Modal.module.css';
+import React, { Component } from 'react';
+import css from './Modal.module.css';
 
-export function Modal() {
-    return (
-        <div className="Modal">
-            <div className="modal">
-                <img src="" alt="" />
+class Modal extends Component {
+    state = {};
+
+    //*слухач на клік по клавіатурі
+    componentDidMount() {
+        window.addEventListener('keydown', this.keydownClick);
+    }
+
+    //*видалення слухача на клік по клавіатурі
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.keydownClick);
+    }
+
+    //*закриття модалки по кліку на Escape
+    keydownClick = evt => {
+        // console.log('evt.code', evt.code);
+        if (evt.code === 'Escape') {
+            // console.log('esc');
+
+            this.props.onClose();
+        }
+    };
+
+    //*закриття модалки по кліку на overly
+    backdropClick = evt => {
+        // console.log('evt.target', evt.target);
+        // console.log('evt.currentTarget', evt.currentTarget);
+        if (evt.target === evt.currentTarget) {
+            this.props.onClose();
+        }
+    };
+
+    render() {
+        const {
+            onClick,
+            image: { alt, url },
+        } = this.props;
+
+        return (
+            <div className={css.overlay} onClick={this.backdropClick}>
+                <div className={css.modal}>
+                    <img src={url} alt={alt} onClick={onClick} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default Modal;
