7b776a2c2ea92b23bc296574dbc9119bb115b113
Grek Maria <grekmariaa@gmail.com>
2022-10-22 16:39:37 +0300

    show more btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,113 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Button } from './Button/Button';
 import { ImageGallery } from './imageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
-// import { Modal } from './Modal/Modal';
+import Modal from './Modal/Modal';
 import { Searchbar } from './searchbar/Searchbar';
+import { ImageGalleryItem } from './imageGalleryItem/ImageGalleryItem';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 class App extends Component {
-    state = {};
+    state = {
+        search: '',
+        images: [],
+        page: 1,
+        loading: false,
+        showModal: false,
+        modal: {},
+    };
 
-    // отримання значення інпуту пошуку
+    //*отримання значення інпуту пошуку
     trackingSearch = evt => {
         evt.preventDefault();
 
         const form = evt.currentTarget;
         const searchValue = form.elements.search.value;
-        console.log('searchValue', searchValue);
+        // console.log('searchValue', searchValue);
+        this.setState({ search: searchValue });
 
         form.reset();
     };
 
+    // стадія оновлення
+    async componentDidUpdate(prevProps, prevState) {
+        const prevSearch = prevState.search;
+        const currentSearch = this.state.search;
+        const prevPage = prevState.page;
+        const currentPage = this.state.page;
+
+        if (prevSearch !== currentSearch || prevPage !== currentPage) {
+            this.setState({ loading: true });
+
+            this.Api();
+        }
+    }
+
+    //! винести Api в окремий компонент?
+    Api = async () => {
+        try {
+            const response = await axios.get('', {
+                params: {
+                    key: '29743747-4d974b8d370b5a5c48adadad9',
+                    q: this.state.search,
+                    image_type: 'photo',
+                    orientation: 'horizontal',
+                    per_page: '12',
+                    page: this.state.page,
+                },
+            });
+
+            this.setState({ images: response.data.hits });
+        } catch (error) {
+            console.log('error', error);
+        } finally {
+            this.setState({ loading: false });
+        }
+    };
+
+    // toggleModal = evt => {
+    //     this.setState({ modal: { alt: evt.target.alt, url: evt.currentTarget.dataset.large } });
+
+    //     const { showModal } = this.state;
+
+    //     this.setState({ showModal: !showModal });
+    // };
+
+    openModal = evt => {
+        this.setState({ modal: { alt: evt.target.alt, url: evt.currentTarget.dataset.large } });
+
+        this.setState({ showModal: true });
+    };
+
+    closeModal = evt => {
+        this.setState({ showModal: false });
+    };
+
+    loadMoreImages = () => {
+        console.log('click on Load more');
+
+        this.setState(prevState => {
+            return { page: prevState.page + 1 };
+        });
+    };
+
     render() {
+        const { images, showModal, modal, loading } = this.state;
+        console.log('images', images.length > 0);
+
         return (
             <>
                 <Searchbar onSubmit={this.trackingSearch} />
-                <ImageGallery />
-                <Loader />
-                <Button />
-                {/* <Modal/> */}
+                <ImageGallery>
+                    {images.map(image => (
+                        <ImageGalleryItem key={image.id} image={image} onClick={this.openModal} />
+                    ))}
+                </ImageGallery>
+                {loading && <Loader />}
+                {images.length > 0 && <Button onClick={this.loadMoreImages} />}
+                {showModal && <Modal image={modal} onClose={this.closeModal} />}
             </>
         );
     }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,11 @@
 import css from './Button.module.css';
 
-export function Button() {
+export function Button({ onClick }) {
     return (
-        <button type="button" className={css.button}>
-            Завантажити ще
-        </button>
+        <div className={css.buttonContainer}>
+            <button type="button" className={css.button} onClick={onClick}>
+                Load more
+            </button>
+        </div>
     );
 }
